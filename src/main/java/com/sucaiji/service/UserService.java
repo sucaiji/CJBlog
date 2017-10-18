4dbf75c1aa0b43db63ab207f1cdbfc4b1b6dfd10
sufchicken <1241892914@qq.com>
2017-10-18 20:05:03 +0800

    基本完成

---
+++ src/main/java/com/sucaiji/service/UserService.java
@@ -0,0 +1,41 @@
+package com.sucaiji.service;
+
+import com.sucaiji.entity.User;
+
+/**
+ * Created by sucaiji on 2017/10/16.
+ */
+public interface UserService {
+    /**
+     * 创建一个用户
+     * @param count
+     * @param password
+     * @param name
+     */
+    void createUser(String count,String password,String name);
+
+    /**
+     * 验证用户密码是否正确
+     * 返回用户id
+     * 如果错误，返回小于0的状态码
+     * @param count
+     * @param password
+     * @return
+     */
+    Integer judgeUser(String count,String password);
+
+    /**
+     * 获得用户名称 通过userid
+     * @param userId
+     * @return
+     */
+    String getName(Integer userId);
+
+    /**
+     * 验证用户是否有权限
+     * @param userId
+     * @return
+     */
+    Boolean judgeAuthorization(Integer userId);
+
+}
